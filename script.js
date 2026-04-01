d76a580f5b6aeb93e6896f9542f014cc2e99f0bf
Nimra Zulfiqar <nimrazulfiqar002@gmail.com>
2026-04-02 04:06:09 +0500

    updated project

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ pages.forEach(page => {
     });
 });
 
-// Items per page dropdown
-document.getElementById("itemsPerPage").addEventListener("change", function() {
-    alert("Show " + this.value + " items per page");
-    // Here you will reload table data based on selected number
-});
+function toggleSidebar() {
+    document.getElementById("hum-sidebar").classList.toggle("active");
+}
